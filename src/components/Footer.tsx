06174b5151ec0a73516611348372648133c176b2
Hitarth <hitarththakkar2004@gmail.com>
2025-07-09 00:05:42 +0530

    last

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
                 <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
               </svg>
             </a> */}
-            <a href="#" className="h-10 w-10 rounded-full bg-portfolio-primary/20 flex items-center justify-center text-portfolio-primary hover:bg-portfolio-primary hover:text-white transition-colors">
+            <a href="https://drive.google.com/file/d/1GtiQNKLnqpja3nLoI8SavjVn5_HvZwCB/view?usp=sharing" className="h-10 w-10 rounded-full bg-portfolio-primary/20 flex items-center justify-center text-portfolio-primary hover:bg-portfolio-primary hover:text-white transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M6 2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z"></path>
                 <path d="M6 2v16"></path>
